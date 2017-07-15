273de0bb9dd5166e1acec6b0669186cd954ddfb9
Ricky Miller <ricky.miller@gmail.com>
2017-07-15 00:57:44 -0400

    Add seperator

--- app/menus/menu.js
+++ app/menus/menu.js
@@ -19,8 +19,9 @@ module.exports = (createWindow, updatePlugins, getLoadedPluginVersions) => {
   const commands = getKeymaps().commands;
 
   const contextMenuTemplate = () => {
+    const seperator = {type: 'separator'};
     return editMenu(commands).submenu
-    .concat(shellMenu(commands, createWindow).submenu);
+    .concat(seperator, shellMenu(commands, createWindow).submenu);
   };
 
   // expose to plugins
